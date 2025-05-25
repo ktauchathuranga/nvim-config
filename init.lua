76922f45362904cacfce57fd31b7d6cf34e57547
Ashen Chathuranga <ktauchathuranga@gmail.com>
2025-05-25 14:51:39 +0530

    enhancements ane enhacementetete

--- init.lua
+++ init.lua
@@ -33,10 +33,65 @@ local function execute_git_command(cmd, success_msg, error_msg)
     end
 end
 
+-- Helper function to get list of modified/untracked files from git status
+local function get_git_files()
+    local git_root = get_git_root()
+    local output = vim.fn.system('git -C ' .. vim.fn.shellescape(git_root) .. ' status --short 2>/dev/null')
+    if vim.v.shell_error ~= 0 then
+        return {}
+    end
+    local files = {}
+    for line in output:gmatch("[^\r\n]+") do
+        -- Extract file path from git status --short (e.g., " M file.txt" or "?? file.txt")
+        local file = line:match("^%s*%S+%s+(.+)$")
+        if file then
+            table.insert(files, file)
+        end
+    end
+    return files
+end
+
 -- Git keybindings
 vim.keymap.set('n', '<leader>ga', function()
-    execute_git_command('add .', 'Git add successful', 'Git add failed')
-end, { noremap = true, desc = "Git add ." })
+    execute_git_command('add .', 'Git add all successful', 'Git add all failed')
+end, { noremap = true, desc = "Git add all files" })
+
+vim.keymap.set('n', '<leader>gf', function()
+    if not is_git_repo() then
+        print("Not a git repository")
+        return
+    end
+    local files = get_git_files()
+    local current_file = vim.fn.expand('%:t')
+    if current_file ~= '' and not vim.tbl_contains(files, current_file) then
+        table.insert(files, current_file) -- Add current file if not in git status
+    end
+    if #files == 0 then
+        print("No modified or untracked files found")
+        if current_file ~= '' then
+            execute_git_command('add ' .. vim.fn.shellescape(current_file), 'Git add ' .. current_file .. ' successful', 'Git add ' .. current_file .. ' failed')
+        else
+            print("No file open to add")
+        end
+        return
+    end
+    vim.ui.select(files, {
+        prompt = 'Select file to git add (default: ' .. (current_file ~= '' and current_file or 'none') .. '):',
+        format_item = function(item)
+            return item
+        end,
+    }, function(choice)
+        if choice == nil then
+            if current_file ~= '' then
+                execute_git_command('add ' .. vim.fn.shellescape(current_file), 'Git add ' .. current_file .. ' successful', 'Git add ' .. current_file .. ' failed')
+            else
+                print("Git add aborted: No file selected")
+            end
+            return
+        end
+        execute_git_command('add ' .. vim.fn.shellescape(choice), 'Git add ' .. choice .. ' successful', 'Git add ' .. choice .. ' failed')
+    end)
+end, { noremap = true, desc = "Git add specific file" })
 
 vim.keymap.set('n', '<leader>gc', function()
     if not is_git_repo() then
@@ -82,6 +137,7 @@ vim.keymap.set('n', '<leader>qa', ':quitall<CR>', { noremap = true, silent = tru
 vim.keymap.set('n', '<leader>q!', ':quit!<CR>', { noremap = true, silent = true, desc = "Quit without saving" })
 vim.keymap.set('n', '<leader>wqa', ':wqa<CR>', { noremap = true, silent = true, desc = "Write all and quit" })
 vim.keymap.set('n', '<leader>wa', ':wall<CR>', { noremap = true, silent = true, desc = "Write all buffers" })
+vim.keymap.set('n', '<leader>nf', ':NewFile<CR>', { noremap = true, desc = "Create new file in Git root" })
 
 -- Function to check if in a Git repository
 function is_git_repo()
@@ -151,3 +207,28 @@ function add_git_remote()
         'Added remote ' .. remote_name .. ' with URL ' .. remote_url,
         'Failed to add remote ' .. remote_name)
 end
+
+vim.api.nvim_create_user_command('NewFile', function(opts)
+    local git_root = get_git_root()
+    local filename = opts.args
+    if filename == '' then
+        filename = vim.fn.input('Enter filename: ')
+    end
+    if filename == '' then
+        print("No filename provided")
+        return
+    end
+    -- Construct the full path by joining git_root and filename
+    local full_path = git_root .. '/' .. filename
+    -- Get the directory path (parent directories of the file)
+    local dir_path = vim.fn.fnamemodify(full_path, ':h')
+    -- Create parent directories if they don't exist
+    if vim.fn.isdirectory(dir_path) == 0 then
+        vim.fn.mkdir(dir_path, 'p')
+        print("Created directories: " .. dir_path)
+    end
+    -- Open the file for editing
+    vim.cmd("edit " .. vim.fn.fnameescape(full_path))
+    print("Creating new file: " .. full_path)
+end, { nargs = '?' })
+
